4db126cc7276489e91b85344261915e2ee0572d4
John Pappas <ecilon@stratag.com>
2018-07-25 08:25:29 -0500

    gb3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,21 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+
+            var fifth = (int)Math.Ceiling(averageGrade);
+            int nHigher = 0;
+            foreach (Student student in Students)
+                if (student.AverageGrade > averageGrade) nHigher++;
+            var numGrade = (int)Math.Floor((double)nHigher / fifth) + 1;
+            if (numGrade < 5)
+                return Convert.ToChar((byte)(numGrade + 64));
+            else
+                return 'F';
+        }
     }
 }
